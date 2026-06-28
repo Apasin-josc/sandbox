301eb74cf76a14128c4e863a02b5872b64e59c81
josescoppola <joseomar_coppola@hotmail.com>
2026-06-28 17:43:33 +0200

    lightning the grill with some javascript knowledge

---
+++ phase_0/hello.js
@@ -0,0 +1,10 @@
+const name = "Jose"
+console.log(`hello from Node, ${name}`);
+
+const habit = { name: "Read", streak: 0 };
+habit.streak = 5;       
+console.log(habit);
+
+const habits = ["read", "run"];
+habits.push("meditate"); 
+console.log(habits);
