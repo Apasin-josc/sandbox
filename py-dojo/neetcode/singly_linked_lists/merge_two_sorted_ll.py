a68d15f07ac951e1a8573475cb05d3f5d4178b2a
José Omar <joseomar_coppola@hotmail.com>
2026-08-01 13:01:13 +0200

    adding a problem of merging two sorted linked list to the sandbox

--- py-dojo/neetcode/singly_linked_lists/merge_two_sorted_ll.py
+++ py-dojo/neetcode/singly_linked_lists/merge_two_sorted_ll.py
@@ -3,8 +3,34 @@ from singly_linked_list import ListNode, build
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        print(list1)
-        print(list2)
-
+        
+        dummy, curr = ListNode(0), ListNode(0)
+        dummy.next = curr
+        
+        
+        while list1 and list2:
+            
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+                
+            else:
+                curr.next = list2
+                list2 = list2.next
+        
+            curr = curr.next
+            
+        while list1:
+            curr.next = list1
+            list1 = list1.next
+            curr = curr.next
+            
+        while list2:
+            curr.next = list2
+            list2 = list2.next
+            curr = curr.next
+            
+        return dummy.next.next
+        
 
 print(Solution().mergeTwoLists(build([1, 2, 4]), build([1, 3, 5])))
