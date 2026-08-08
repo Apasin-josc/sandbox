7835c521677ff760d1625eb03e1f92d56c5145bb
José Omar <joseomar_coppola@hotmail.com>
2026-08-08 18:13:44 +0200

    leaving a cool sandbox for tomorrow tree grinding

--- py-dojo/neetcode/trees/invert_binary_tree.py
+++ py-dojo/neetcode/trees/invert_binary_tree.py
@@ -4,9 +4,9 @@ from typing import Optional
 
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        pass
+        print(root)
 
 
 print(Solution().invertTree(build([1, 2, 3, 4, 5, 6, 7]))) # esperado: [1, 3, 2, 7, 6, 5, 4]
-print(Solution().invertTree(build([]))) # esperado: []
+#print(Solution().invertTree(build([]))) # esperado: []
 
